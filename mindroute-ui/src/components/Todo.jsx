b229cd97f038230888f8d53974b359450e7e7393
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-11-28 19:05:55 +0530

    MindRoute Frontend Setup

---
+++ mindroute-ui/src/components/Todo.jsx
@@ -0,0 +1,49 @@
+import { useSelector } from "react-redux";
+import AddForm from "./AddForm";
+import { useDispatch } from "react-redux";
+import { deleteTodo } from "../features/todo/todoSlice";
+import { marksAsDone } from "../features/todo/todoSlice";
+
+export default function Todo() {
+  const todos = useSelector((state) => state.todos);
+  console.log(todos);
+  const dispatch = useDispatch();
+
+  const clickHandler = (id) => {
+    //console.log("delete", id);
+    dispatch(deleteTodo(id));
+  };
+
+  const markAsDone = (id) => {
+    //console.log("mark as done", id);
+    dispatch(marksAsDone(id));
+  };
+
+  return (
+    <>
+      <AddForm />
+      <h2>Todo List App</h2>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.isDone ? (
+              <span
+                style={{ textDecorationLine: "line-through", color: "red" }}
+              >
+                {todo.task}
+              </span>
+            ) : (
+              <span>{todo.task}</span>
+            )}
+            &nbsp;&nbsp;&nbsp;
+            <button onClick={() => clickHandler(todo.id)}>Delete</button>
+            &nbsp;&nbsp;
+            {!todo.isDone && (
+              <button onClick={() => markAsDone(todo.id)}>Mark As Done</button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
