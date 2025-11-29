e028a6d1277cce783fe48b34aa8dfc812b6b3bc0
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-11-29 18:27:39 +0530

    Connecting frontend and backend

--- mindroute-ui/src/features/todo/todoSlice.js
+++ mindroute-ui/src/features/todo/todoSlice.js
@@ -1,33 +1,77 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 import { getOrCreateUserId } from "../../utils/userId.js";
+import {
+  getTodos,
+  addTodo,
+  marksAsDoneTodo,
+  deleteTodo,
+} from "../../service/todos.js";
 
 const initialState = {
   userId: getOrCreateUserId(),
   todos: [],
+  loading: false,
 };
 
 export const todoSlice = createSlice({
   name: "todo",
   initialState,
   reducers: {
-    addTodo: (state, action) => {
-      const newTodo = {
-        id: nanoid(),
-        task: action.payload,
-        isDone: false,
-      };
-      state.todos.push(newTodo);
+    getTodos: async (state, action) => {
+      state.loading = true;
+      try {
+        const todos = await getTodos(state.userId);
+        state.todos = todos;
+      } catch (err) {
+        state.todos = [];
+      } finally {
+        state.loading = false;
+      }
     },
-    deleteTodo: (state, action) => {
-      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    addTodo: async (state, action) => {
+      state.loading = true;
+      try {
+        const newTodo = {
+          id: nanoid(),
+          task: action.payload,
+          isDone: false,
+        };
+        const response = await addTodo(userId, newTodo);
+        state.todos.push(newTodo);
+      } catch (err) {
+        state.todos = state.todos;
+      } finally {
+        state.loading = false;
+      }
     },
-    marksAsDone: (state, action) => {
-      state.todos = state.todos.map((todo) => {
-        if (todo.id === action.payload) {
-          return { ...todo, isDone: true };
-        }
-        return todo;
-      });
+    deleteTodo: async (state, action) => {
+      state.loading = true;
+      try {
+        const todoId = action.payload;
+        const response = await deleteTodo(user, todoId);
+        state.todos = state.todos.filter((todo) => todo.id !== todoId);
+      } catch (err) {
+        state.todos = state.todos;
+      } finally {
+        state.loading = false;
+      }
+    },
+    marksAsDone: async (state, action) => {
+      state.loading = true;
+      try {
+        const todoId = action.payload;
+        const response = await marksAsDoneTodo(userId, todoId);
+        state.todos = state.todos.map((todo) => {
+          if (todo.id === todoId) {
+            return { ...todo, isDone: true };
+          }
+          return todo;
+        });
+      } catch (err) {
+        state.todos = state.todos;
+      } finally {
+        state.loading = false;
+      }
     },
   },
 });
---
+++ mindroute-ui/src/service/todos.js
@@ -0,0 +1,7 @@
+export const getTodos = async (userId) => {};
+
+export const addTodo = async (userId, todo) => {};
+
+export const marksAsDoneTodo = async (userId, todoId) => {};
+
+export const deleteTodo = async (userId, todoId) => {};
