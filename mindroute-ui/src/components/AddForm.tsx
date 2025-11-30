27221e679ba5848fdfe24f5b1a82f2cce87a36dc
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-11-30 12:27:55 +0530

    UI and Functionality Completed

--- mindroute-ui/src/components/AddForm.tsx
+++ mindroute-ui/src/components/AddForm.tsx
@@ -40,7 +40,7 @@ export default function AddForm() {
       ></input>
       <button
         type="submit"
-        className="flex items-center justify-center px-5 py-3 bg-blue-600 rounded-xl font-semibold text-white hover:bg-blue-700 transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed"
+        className="flex items-center justify-center px-5 py-3 bg-blue-600 rounded-xl font-semibold text-white hover:bg-blue-700 transition-all duration-200 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
         disabled={!task.trim() || addLoading}
       >
         {addLoading ? (
