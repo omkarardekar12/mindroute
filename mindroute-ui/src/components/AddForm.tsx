c2cadbd743c80ea7a6b62b39bd4e51d5a407587f
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-11-30 11:01:57 +0530

    Added Form

--- mindroute-ui/src/components/AddForm.tsx
+++ mindroute-ui/src/components/AddForm.tsx
@@ -1,21 +1,21 @@
-import { useState } from "react";
-import type { FormEvent } from "react";
-import { useDispatch } from "react-redux";
-import type { AppDispatch } from "../app/store.ts";
+import { useState, type FormEvent } from "react";
+import { useAppDispatch, useAppSelector } from "../app/hooks.ts";
 import { addTodoAsync } from "../features/todo/todoSlice.ts";
 import toast from "react-hot-toast";
+import ButtonLoading from "./ButtonLoading.tsx";
 
 export default function AddForm() {
   const [task, setTask] = useState("");
-  const dispatch = useDispatch<AppDispatch>();
+  const dispatch = useAppDispatch();
+  const addLoading = useAppSelector((state) => state.todo.addLoading);
 
   const submitHandler = async (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
+
     if (!task.trim()) {
       toast.error("Task cannot be empty!");
       return;
     }
-    //console.log(task);
 
     try {
       await dispatch(addTodoAsync(task)).unwrap();
@@ -35,14 +35,22 @@ export default function AddForm() {
         type="text"
         value={task}
         onChange={(e) => setTask(e.target.value)}
+        placeholder="Enter your task..."
         className="flex-1 px-4 py-3 bg-gray-900 text-white rounded-xl outline-none placeholder-gray-500border border-gray-700 focus:border-blue-500 focus:ring-2 focus:ring-blue-600 transition"
       ></input>
       <button
         type="submit"
         className="px-5 py-3 bg-blue-600 rounded-xl font-semibold text-white hover:bg-blue-700 transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed"
-        disabled={!task.trim()}
+        disabled={!task.trim() || addLoading}
       >
-        Add Task
+        {addLoading ? (
+          <>
+            <ButtonLoading />
+            <span>Add Task</span>
+          </>
+        ) : (
+          <span>Add Task</span>
+        )}
       </button>
     </form>
   );
