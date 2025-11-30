b96ba3d7ce0726f12e865ea97d8053415515e8b4
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-11-30 11:14:57 +0530

    Todo UI and Functionality

--- mindroute-ui/src/components/Todo.tsx
+++ mindroute-ui/src/components/Todo.tsx
@@ -1,7 +1,5 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import type { RootState, AppDispatch } from "../app/store.ts";
+import { useAppDispatch, useAppSelector } from "../app/hooks.ts";
 import AddForm from "./AddForm.tsx";
 import { FaTasks } from "react-icons/fa";
 import { BsListTask } from "react-icons/bs";
@@ -14,10 +12,14 @@ import {
 } from "../features/todo/todoSlice.ts";
 import toast from "react-hot-toast";
 import Loading from "./Loading.tsx";
+import ButtonLoading from "./ButtonLoading.tsx";
+import type { Todo } from "../service/todos.ts";
 
 export default function Todo() {
-  const { todos, loading } = useSelector((state: RootState) => state.todo);
-  const dispatch = useDispatch<AppDispatch>();
+  const { todos, loading, markLoading, deleteLoading } = useAppSelector(
+    (state) => state.todo
+  );
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(fetchTodos());
@@ -27,7 +29,7 @@ export default function Todo() {
     try {
       await dispatch(deleteTodoAsync(id)).unwrap();
       toast.success("Task deleted");
-    } catch (err) {
+    } catch {
       toast.error("Failed to delete task");
     }
   };
@@ -36,64 +38,82 @@ export default function Todo() {
     try {
       await dispatch(markAsDoneAsync(id)).unwrap();
       toast.success("Marked as done");
-    } catch (error) {
+    } catch {
       toast.error("Failed to mark as done");
     }
   };
 
-  return <Loading />;
+  if (loading) {
+    return <Loading />;
+  }
 
-  // if (loading) {
-  //   return <Loading />;
-  // }
-
-  // return (
-  //   <div className="w-full h-full flex flex-col items-center justify-center bg-gradient-to-b from-black via-gray-950 to-gray-950 text-white p-4">
-  //     <AddForm />
-  //     <div className="w-full md:w-[60%] flex flex-col justify-center p-4 gap-4 text-xl">
-  //       {todos.length > 0 ? (
-  //         todos.map((todo) => (
-  //           <div
-  //             key={todo.id}
-  //             className="w-full flex flex-row justify-between flex-wrap"
-  //           >
-  //             {todo.isDone ? (
-  //               <div className="flex items-center line-through text-green-500 opacity-70 gap-3">
-  //                 <BsListTask className="size-6" />
-  //                 <span>{todo.task}</span>
-  //               </div>
-  //             ) : (
-  //               <div className="flex items-center gap-3">
-  //                 <BsListTask className="size-6" />
-  //                 <span>{todo.task}</span>
-  //               </div>
-  //             )}
-  //             <div className="flex flex-row gap-2 flex-wrap">
-  //               {!todo.isDone && (
-  //                 <button
-  //                   className="flex items-center gap-2 px-4 py-2 bg-green-600 rounded-lg text-sm hover:bg-green-700 transition-all duration-200 cursor-pointer"
-  //                   onClick={() => markAsDone(todo.id)}
-  //                 >
-  //                   <IoMdCheckmarkCircleOutline className="size-5" />
-  //                   <span>Mark As Done</span>
-  //                 </button>
-  //               )}
-  //               <button
-  //                 className="flex items-center gap-2 px-4 py-2 bg-red-600 rounded-lg text-sm hover:bg-red-700 transition-all duration-200 cursor-pointer"
-  //                 onClick={() => handleDelete(todo.id)}
-  //               >
-  //                 <IoMdCloseCircleOutline className="size-5" />
-  //                 <span>Delete</span>
-  //               </button>
-  //             </div>
-  //           </div>
-  //         ))
-  //       ) : (
-  //         <p className="text-md text-gray-400 flex flex-row items-center justify-center gap-3">
-  //           <FaTasks className="size-5" /> Current No Task
-  //         </p>
-  //       )}
-  //     </div>
-  //   </div>
-  // );
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center bg-gradient-to-b from-black via-gray-950 to-gray-950 text-white p-4">
+      <AddForm />
+      <div className="w-full md:w-[60%] flex flex-col justify-center p-4 gap-4 text-xl">
+        {todos.length > 0 ? (
+          todos.map((todo: Todo) => (
+            <div
+              key={todo.id}
+              className="w-full flex flex-row justify-between flex-wrap"
+            >
+              {todo.isDone ? (
+                <div className="flex items-center line-through text-green-500 opacity-70 gap-3">
+                  <BsListTask className="size-6" />
+                  <span>{todo.task}</span>
+                </div>
+              ) : (
+                <div className="flex items-center gap-3">
+                  <BsListTask className="size-6" />
+                  <span>{todo.task}</span>
+                </div>
+              )}
+              <div className="flex flex-row gap-2 flex-wrap">
+                {!todo.isDone && (
+                  <button
+                    className="flex items-center gap-2 px-4 py-2 bg-green-600 rounded-lg text-sm hover:bg-green-700 transition-all duration-200 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+                    onClick={() => markAsDone(todo.id)}
+                    disabled={markLoading}
+                  >
+                    {markLoading ? (
+                      <>
+                        <ButtonLoading />
+                        <span>Marking...</span>
+                      </>
+                    ) : (
+                      <>
+                        <IoMdCheckmarkCircleOutline className="size-5" />
+                        <span>Mark As Done</span>
+                      </>
+                    )}
+                  </button>
+                )}
+                <button
+                  className="flex items-center gap-2 px-4 py-2 bg-red-600 rounded-lg text-sm hover:bg-red-700 transition-all duration-200 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+                  onClick={() => handleDelete(todo.id)}
+                  disabled={deleteLoading}
+                >
+                  {deleteLoading ? (
+                    <>
+                      <ButtonLoading />
+                      <span>Deleting...</span>
+                    </>
+                  ) : (
+                    <>
+                      <IoMdCloseCircleOutline className="size-5" />
+                      <span>Delete</span>
+                    </>
+                  )}
+                </button>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className="text-md text-gray-400 flex flex-row items-center justify-center gap-3">
+            <FaTasks className="size-5" /> Current No Task
+          </p>
+        )}
+      </div>
+    </div>
+  );
 }
