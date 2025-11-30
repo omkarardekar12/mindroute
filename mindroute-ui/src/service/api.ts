4ba30d498a6a83e59dee3c1263bf8f518c739540
OmkarArdekar12 <omkarardekar12@gmail.com>
2025-11-30 10:42:59 +0530

    Created Axios Instance

--- mindroute-ui/src/service/api.ts
+++ mindroute-ui/src/service/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = import.meta.env.BACKEND_URL as string;
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL as string;
 
 const api = axios.create({
   baseURL: `${BACKEND_URL}/api`,
